6c64f948eaa27eb10ed39c9e505f9a9ef85701a0
curiositypaths <curiositypaths@users.noreply.github.com>
2016-12-16 14:42:49 -0500

    Done

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params['pirate']['name'],params['pirate']['height'],params['pirate']['weight'])
+      @ships = params[:pirate][:ships].map do |ship|
+        a = Ship.new(ship['name'],ship['type'],ship['booty'])
+      end
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  # Pirates should have a name, weight, and height. You will also need a class method that returns all the pirates.
+  attr_accessor :name, :weight, :height
+  @@pirates = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
